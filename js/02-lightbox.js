618d80b4c338d0bd7dc7b9b211d89b6bd13a5937
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-04-24 18:12:51 +0300

    edit 2 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,32 @@ import {galleryItems} from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery');
+
+const img = galleryItems
+  .map((element) => {
+    const {original, preview, description} = element;
+    return `<a class='gallery__item' href=${original}>
+          <img
+            class='gallery__image'
+            src=${preview}
+            alt=${description}
+          />
+        </a>`;
+  })
+  .join('');
+
+galleryDiv.innerHTML = img;
+
+const lightbox = new SimpleLightbox('.gallery a', {});
+
+const onLinkClick = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName === '.gallery__image') {
+    return;
+  }
+};
+
+galleryDiv.addEventListener('click', onLinkClick);
